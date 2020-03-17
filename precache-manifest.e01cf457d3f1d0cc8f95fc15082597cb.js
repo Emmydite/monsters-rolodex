03e19dd6b3634f36ff7fe7a756500387441bf000
emmysteve <emmacomessi@gmail.com>
2020-03-17 21:48:01 +0100

    Updates

---
+++ precache-manifest.e01cf457d3f1d0cc8f95fc15082597cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "173162d60cbeb2df3f1df9be6587f592",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "15af361b1c3ef057b2a4",
+    "url": "/monsters-rolodex/static/css/main.5645e509.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15af361b1c3ef057b2a4",
+    "url": "/monsters-rolodex/static/js/main.a771028c.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
